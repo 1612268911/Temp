c53af743993a8becedd62c5190d6022a69adfa1e
jj <1612268911@qq.com>
2018-08-22 12:27:56 +0800

    1

--- testlist/src/main/resources/sql/create.sql
+++
@@ -1,4 +0,0 @@
-create table class(
-  cid char(10) primary key,
-  name VARCHAR(50)
-);
--- testlist/src/main/resources/sql/data.sql
+++
@@ -1,3 +0,0 @@
-insert into class(cid,name) values ('1','java1班');
-insert into class(cid,name) values ('2','java2班');
-insert into class(cid,name) values ('3','java3班');
